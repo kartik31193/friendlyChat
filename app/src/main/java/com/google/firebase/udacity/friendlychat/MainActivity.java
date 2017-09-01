b910327a41cc87eaa58e899500d37a77e97b9b7e
Kartik Budholiya <kartik31193@gmail.com>
2017-09-01 10:13:08 +0530

    childadded

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -152,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
     }
 
     @Override
